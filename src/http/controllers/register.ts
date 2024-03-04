0bc178f820f2c0e1d91fd48b78e921d7b52131fe
Victor Nogueira <vmnogueira.ti@gmail.com>
2024-03-03 22:19:54 -0300

    Adds Custom Global Error Handler

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -24,7 +24,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       })
     }
 
-    return reply.status(500).send() // TODO: fix me
+    throw error
   }
 
   return reply.status(201).send()
