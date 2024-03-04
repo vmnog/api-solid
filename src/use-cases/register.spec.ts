673e14a6d763ab354c072d823070123855468bc4
Victor Nogueira <vmnogueira.ti@gmail.com>
2024-03-03 22:38:53 -0300

    Unit test password hashing

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,6 +1,35 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('should work', () => {
-  expect(true).toBeTruthy()
-  expect(false).toBeFalsy()
+describe('Register Use Case', () => {
+  it('should hash user password on registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+      async findByEmail() {
+        return null
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
