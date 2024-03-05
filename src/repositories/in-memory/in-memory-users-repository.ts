c9f075792f6bfb26fd35a26a3df3b4bb9027dbec
Victor Nogueira <vmnogueira.ti@gmail.com>
2024-03-05 01:12:00 -0300

    Creates Check Ins Repository and UseCase

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,12 +1,13 @@
 import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
 
   async create(data: Prisma.UserCreateInput) {
     const user: User = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
@@ -19,7 +20,7 @@ export class InMemoryUsersRepository implements UsersRepository {
   }
 
   async findById(id: string): Promise<User | null> {
-    const user = this.items.find((item) => item.email === id)
+    const user = this.items.find((item) => item.id === id)
 
     if (!user) {
       return null
---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,22 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-checkins-repository'
+import { CheckInUseCase } from './check-in'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: CheckInUseCase
+
+describe('CheckIns Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new CheckInUseCase(checkInsRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      userId: 'user-id',
+      gymId: 'gym-id',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
