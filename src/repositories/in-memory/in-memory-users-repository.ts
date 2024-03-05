5f7363e3ec86d3ed1fd22d2fc3246eb29f372da4
Victor Nogueira <vmnogueira.ti@gmail.com>
2024-03-05 00:03:15 -0300

    Create get user profile use case

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -18,6 +18,16 @@ export class InMemoryUsersRepository implements UsersRepository {
     return user
   }
 
+  async findById(id: string): Promise<User | null> {
+    const user = this.items.find((item) => item.email === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
   async findByEmail(email: string): Promise<User | null> {
     const user = this.items.find((item) => item.email === email)
 
