89e19829beede03d2fa42d585a88b3ab25380389
Victor Nogueira <vmnogueira.ti@gmail.com>
2024-03-16 00:01:03 -0300

    Validates Session JWT on Authenticate

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -16,7 +16,18 @@ export async function authenticate(
 
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({ email, password })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id
+        }
+      }
+    )
+
+    return reply.status(200).send({ token })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({
@@ -26,6 +37,4 @@ export async function authenticate(
 
     throw error
   }
-
-  return reply.status(200).send()
 }
--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,8 +1,8 @@
 import { expect, describe, it, beforeEach } from 'vitest'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
-import { AuthenticateUseCase } from './authenticate'
 import { hash } from 'bcryptjs'
 import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { AuthenticateUseCase } from './authenticate'
 
 let usersRepository: InMemoryUsersRepository
 let sut: AuthenticateUseCase
